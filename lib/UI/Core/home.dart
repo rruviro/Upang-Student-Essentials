0c68fd539285f439ad1be7b7d0ab500339f90b73
rruviro <j1louise.v@gmail.com>
2024-08-06 00:27:00 +0800

    goodnight dudies

--- lib/UI/Core/home.dart
+++ lib/UI/Core/home.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 class HomeBase extends StatelessWidget {
@@ -8,6 +9,7 @@ class HomeBase extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Upang Student Essentials',
       home: HomeScreen()
     );
   }
@@ -92,6 +94,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             onPressed: () {},
           ),
+          SizedBox(width: 15),
         ],
         backgroundColor: Colors.white,
         elevation: 0,
@@ -112,34 +115,123 @@ class _HomeScreenState extends State<HomeScreen> {
                   children: <Widget>[
                     Text(
                       'Departments',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 15,
-                        fontFamily: 'fonts/Inter-Regular.ttf',
-                        fontWeight: FontWeight.w600
+                      style: GoogleFonts.inter(
+                        textStyle: TextStyle(
+                          fontSize: 15,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w600
+                        ),
                       ),
                     ),
                     SizedBox(height: 3),
                     Text(
                       'Choose your perspective department for --',
-                      style: TextStyle(
-                        color: Colors.black54,
-                        fontSize: 13,
-                        fontFamily: 'fonts/Inter-Regular.ttf',
-                        fontWeight: FontWeight.w500
+                      style: GoogleFonts.inter(
+                        textStyle: TextStyle(
+                          fontSize: 13,
+                          color: Color.fromARGB(146, 0, 0, 0),
+                          fontWeight: FontWeight.w500
+                        ),
                       ),
                     ),
-                    SizedBox(height: 3), 
+                    SizedBox(height: 10), 
                     ListBody(
-                      children: [
+                      children: <Widget>[
                         Padding(
-                          padding: EdgeInsets.all(8.0),
+                          padding: EdgeInsets.only(
+                            top: 8.0,
+                            bottom: 8.0,
+                          ),
                           child: Container(
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(10),
-                              color: Color.fromARGB(255, 14, 170, 113)
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color.fromARGB(255, 14, 170, 113),
+                                  Color.fromARGB(255, 14, 170, 113),
+                                ],
+                                stops: [
+                                  0.60,
+                                  0.90
+                                ],
+                              ),
+                              // color: Color.fromARGB(255, 14, 170, 113),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color.fromARGB(255, 0, 0, 0).withOpacity(0.5),
+                                  blurRadius: 5,
+                                  offset: Offset(1, 8),
+                                ),
+                              ],
+                              // image: DecorationImage(
+                              //   image: AssetImage('assets/vanguards.png'),
+                              //   fit: BoxFit.cover, 
+                              // ),
                             ),
-                            height: 150,
+                            height: 170,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                    top: 20.0,
+                                    left: 30.0
+                                  ),
+                                  child: Text(
+                                    'Health Sciences',
+                                    style: GoogleFonts.inter(
+                                      textStyle: TextStyle (
+                                        fontSize: 16,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                    top: 5.0,
+                                    left: 30.0
+                                  ),
+                                  child: Text(
+                                    'Courses :',
+                                    style: GoogleFonts.inter(
+                                      textStyle: TextStyle (
+                                        fontSize: 10,
+                                        color: Colors.white54,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                    top: 60.0,
+                                    left: 30.0
+                                  ),
+                                  child: Row(
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.arrow_circle_right_outlined,
+                                        color: Colors.white54,
+                                        size: 20
+                                      ),
+                                      SizedBox(width: 10),
+                                      Text(
+                                        'Show more courses',
+                                        style: GoogleFonts.inter(
+                                          textStyle: TextStyle (
+                                            fontSize: 10,
+                                            color: Colors.white54,
+                                            fontWeight: FontWeight.w200,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ],
@@ -151,15 +243,28 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      bottomNavigationBar: SalomonBottomBar(
-        backgroundColor: Color.fromARGB(255, 14, 170, 113),
-        items: itemsBN,
-        currentIndex: _currentIndex,
-        duration: Duration(seconds: 1),
-        onTap: (index) => setState(() {
-          _currentIndex = index;
-        }),
-      ),
+      bottomNavigationBar: Container (
+        color: Color.fromARGB(255, 14, 170, 113),
+        child: Stack(
+          children: <Widget>[
+            Card(
+              margin: const EdgeInsets.only(
+                right: 120.0,
+                left: 120.0
+              ),
+              child: SalomonBottomBar(
+                backgroundColor: Color.fromARGB(255, 14, 170, 113),
+                items: itemsBN,
+                currentIndex: _currentIndex,
+                duration: Duration(seconds: 1),
+                onTap: (index) => setState(() {
+                  _currentIndex = index;
+                })
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
